0ac008f9eca98d35f4346df67344c8c697b5a738
yzhbankov <yzhbankov@redpointpositioning.com>
2023-04-13 19:24:30 +0000

    Disable same site cookie rule

--- apps/server/src/auth/auth.controller.ts
+++ apps/server/src/auth/auth.controller.ts
@@ -19,7 +19,7 @@ export class AuthController {
 
     res.cookie('access_token', token, {
       maxAge: 2592000000,
-      sameSite: true,
+      sameSite: false,
       secure: false,
     });
     res.status(HttpStatus.OK).send();
