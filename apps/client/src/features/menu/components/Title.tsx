436508ca211b2ee151d76b9861e5f39f607bfa58
yzhbankov <yzhbankov@redpointpositioning.com>
2023-04-15 17:31:18 +0000

    Refactor Menu component. Move components into one file

--- apps/client/src/features/menu/components/Title.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export function Title() {
-    return <div className="text-center text-2xl font-semibold whitespace-nowrap dark:text-white">Bookmarks</div>;
-}
-
-Title.propTypes = {};
-
-Title.defaultProps = {};
