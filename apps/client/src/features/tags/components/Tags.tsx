c4cda0145e51bdbebb02de831399cc17be093062
yzhbankov <yzhbankov@redpointpositioning.com>
2023-04-15 11:18:52 +0000

    Add example dialog

--- apps/client/src/features/tags/components/Tags.tsx
+++ apps/client/src/features/tags/components/Tags.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { ITagsFetch, useFetchTags } from '../hooks';
+import { TagCreateModal } from './TagCreateModal';
 
 export function Tags() {
     const tagsFetch: ITagsFetch = useFetchTags();
+    const [openCreateModal, setOpenCreateModal] = useState<boolean>(false);
+
     return (
         <div>
             <div className="text-center">Tags components</div>
@@ -11,6 +14,15 @@ export function Tags() {
             <div>Fetch: {tagsFetch.isFetching}</div>
             <div>Error: {tagsFetch.isError}</div>
             <div>List: {JSON.stringify(tagsFetch.data)}</div>
+            <button
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                onClick={() => {
+                    setOpenCreateModal(true);
+                }}
+            >
+                Create Tag
+            </button>
+            <TagCreateModal isOpen={openCreateModal} handleOpen={(val: boolean) => setOpenCreateModal(val)} />
         </div>
     );
 }
