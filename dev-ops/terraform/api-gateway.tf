174bd4a95c8fa5706c97da72af407f55c739c77d
yzhbankov <yzhbankov@redpointpositioning.com>
2023-08-18 01:42:41 +0000

    Add API Gateway integrations with lambda functions

--- dev-ops/terraform/api-gateway.tf
+++ dev-ops/terraform/api-gateway.tf
@@ -1,6 +1,6 @@
-resource "aws_apigatewayv2_api" "bookmarks-api" {
-  name          = "bookmarks-api"
-  protocol_type = "HTTP"
+resource "aws_api_gateway_rest_api" "bookmarks-api" {
+  name        = "bookmarks-api"
+  description = "Bookmarks API Gateway"
   body = templatefile("${path.module}/../../apps/api-gateway/api.yaml",
     {
       aws_region           = var.AWS_REGION
@@ -12,17 +12,60 @@ resource "aws_apigatewayv2_api" "bookmarks-api" {
     }
   )
 
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
+
   tags = {
     Name = "bookmarks-api-gateway"
   }
 }
 
-resource "aws_apigatewayv2_stage" "default" {
-  api_id      = aws_apigatewayv2_api.bookmarks-api.id
-  name        = "$default"
-  auto_deploy = true
+resource "aws_api_gateway_deployment" "deployment" {
+  rest_api_id = aws_api_gateway_rest_api.bookmarks-api.id
+  stage_name  = "prod"
+}
 
-  tags = {
-    Name = "bookmarks-api-gateway-stage"
-  }
+resource "aws_lambda_permission" "api-gateway-invoke-bookmarks-lambda" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.bookmarks-lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  # The /*/* portion grants access from any method on any resource
+  # within the specified API Gateway.
+  source_arn = "${aws_api_gateway_rest_api.bookmarks-api.execution_arn}/*/*"
+}
+
+resource "aws_lambda_permission" "api-gateway-invoke-tags-lambda" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.tags-lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  # The /*/* portion grants access from any method on any resource
+  # within the specified API Gateway.
+  source_arn = "${aws_api_gateway_rest_api.bookmarks-api.execution_arn}/*/*"
+}
+
+resource "aws_lambda_permission" "api-gateway-invoke-spaces-lambda" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.spaces-lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  # The /*/* portion grants access from any method on any resource
+  # within the specified API Gateway.
+  source_arn = "${aws_api_gateway_rest_api.bookmarks-api.execution_arn}/*/*"
+}
+
+resource "aws_lambda_permission" "api-gateway-invoke-auth-lambda" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.auth-lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  # The /*/* portion grants access from any method on any resource
+  # within the specified API Gateway.
+  source_arn = "${aws_api_gateway_rest_api.bookmarks-api.execution_arn}/*/*"
 }
