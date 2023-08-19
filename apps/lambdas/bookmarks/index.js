b30624942737174bdae0e7e2119e0d80a0a25207
yzhbankov <yzhbankov@redpointpositioning.com>
2023-08-19 23:07:10 +0000

    Add bookmarks labbda function log

--- apps/lambdas/bookmarks/index.js
+++ apps/lambdas/bookmarks/index.js
@@ -1,8 +1,38 @@
-exports.handler = async (event) => {
-    console.log("Hello from bookmarks lambda!");
+const AWS = require('aws-sdk');
+const dynamodb = new AWS.DynamoDB.DocumentClient();
 
-    return {
-        statusCode: 200,
-        body: "Hello from bookmarks lambda!"
-    };
+const DB_TABLE_NAME = 'prod_bookmarks_table';
+
+exports.handler = async (event) => {
+    try {
+        console.log(JSON.stringify(event));
+        // const requestBody = JSON.parse(event.body);
+        //
+        // const params = {
+        //     TableName: DB_TABLE_NAME,
+        //     Item: {
+        //         id: requestBody.id,
+        //         name: requestBody.name,
+        //         description: requestBody.description
+        //     }
+        // };
+        //
+        // await dynamodb.put(params).promise();
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ message: 'Item added to DynamoDB successfully' }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: error.message }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+    }
 };
