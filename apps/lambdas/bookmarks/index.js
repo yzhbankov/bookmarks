681e7d59bfce7f844b8cd4069e793203822fed05
yzhbankov <yzhbankov@redpointpositioning.com>
2023-09-24 20:10:55 +0000

    Add shared utils symlinc to all lambdas

--- apps/lambdas/bookmarks/index.js
+++ apps/lambdas/bookmarks/index.js
@@ -1,13 +1,10 @@
 import { BookmarksRepo, DatabaseClient } from './models/index.js';
 import Controller from './controller/index.js';
-import { print, print2 } from './shared/index.js';
 
 BookmarksRepo.setRepository(new DatabaseClient('prod_bookmarks_table'));
 
 export const handler = async (event) => {
     try {
-        print();
-        print2();
         return Controller(event['httpMethod'], event);
     } catch (error) {
         return {
