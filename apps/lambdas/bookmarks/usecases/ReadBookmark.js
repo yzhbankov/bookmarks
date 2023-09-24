e7610fc0ca22be6d4ddf156c7998c799f7884e5b
yzhbankov <yzhbankov@redpointpositioning.com>
2023-09-24 17:57:37 +0000

    Fix bookmarks get response

--- apps/lambdas/bookmarks/usecases/ReadBookmark.js
+++ apps/lambdas/bookmarks/usecases/ReadBookmark.js
@@ -2,7 +2,6 @@ import { BookmarksRepo } from '../models/index.js';
 
 export class ReadBookmark {
     async execute(params) {
-        const bookmarks = await new BookmarksRepo().readByOwner(params.owner);
-        return { bookmarks }
+        return await new BookmarksRepo().readByOwner(params.owner);
     }
 }
