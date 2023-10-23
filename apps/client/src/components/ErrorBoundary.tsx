949fdf006ef084c1876c76de41c720bc8fa9bfad
yzhbankov <yzhbankov@redpointpositioning.com>
2023-10-23 01:56:52 +0000

    Add errors logging todo

--- apps/client/src/components/ErrorBoundary.tsx
+++ apps/client/src/components/ErrorBoundary.tsx
@@ -10,11 +10,12 @@ function ErrorFallback({ error, resetErrorBoundary }: FallbackProps) {
         <div role="alert">
             <p>Something went wrong:</p>
             <pre>{error.message}</pre>
-            <button onClick={resetErrorBoundary}>Try again</button>
+            <button onClick={resetErrorBoundary}>Something went wrong. Please refresh the page.</button>
         </div>
     );
 }
 
+// todo: send error to the centralized storage
 const myErrorHandler = (error: Error, info: { componentStack: string }) => {
     // Do something with the error
     // E.g. log to an error logging client here
